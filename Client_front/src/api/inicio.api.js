cb0254e83b5f1b7bde316e8531a37d04b2e17dda
PaMaBu <xpamabux@gmail.com>
2023-06-03 16:06:36 -0600

    remove api

--- Client_front/src/api/inicio.api.js
+++ Client_front/src/api/inicio.api.js
@@ -1,45 +1,38 @@
 import axios from 'axios';  
 
-const apiUrl = axios.create({baseURL:"http://192.168.100.4:80/api/"})
+const apiUrl = axios.create({baseURL:"https://pokeapi.co/api/v2/pokemon"})
+// const apiUrl = axios.create({baseURL:"http://192.168.100.4:80/api/"})
 const apiPokemon = axios.create({baseURL:"https://pokeapi.co/api/v2/pokemon"})
 export const get_lista =() => {
-    // return axios.get("http://192.168.100.4/api/monedas/")
     return apiUrl.get("/monedas/");
 }
 
 export const get_detalle =(id) => {
-    // return axios.get("http://192.168.100.4/api/monedas/")
     return apiUrl.get("/monedas/"+id+"/");
 }
 
 export const create_reg =(moneda) => {
-    // return axios.post("http://192.168.100.4/api/monedas/")
     return apiUrl.post("/monedas/",moneda);
 }
 
 export const delete_reg =(id) => {
-    // return axios.post("http://192.168.100.4/api/monedas/")
     return apiUrl.delete("/monedas/"+id+"/");
 }
 
 
 export const update_reg =(id, moneda) => {
-    // return axios.post("http://192.168.100.4/api/monedas/")
     return apiUrl.put("/monedas/"+id+"/",moneda);
 }
 
 export const get_list_proyectos =() => {
-    // return axios.post("http://192.168.100.4/api/monedas/")
     return apiUrl.get("/proyectos/");
 }
 
 export const get_apiPokemon =() => {
-    // return axios.get("http://192.168.100.4/api/monedas/")
     return apiPokemon.get("/");
 }
 
 export const get_apiPokemon_detail=(id_pokemon) => {
-    // return axios.get("http://192.168.100.4/api/monedas/")
     return apiPokemon.get("/"+id_pokemon+"/");
 }
 
